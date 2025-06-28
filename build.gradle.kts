4492c1e832933403f37be1f60827fefb88eed95a
Ernest Aidinov <ernest.aidinov@media-tel>
2025-06-28 20:59:13 +0300

    add jooq generation

--- build.gradle.kts
+++ build.gradle.kts
@@ -2,6 +2,7 @@ plugins {
 	java
 	id("org.springframework.boot") version "3.5.3"
 	id("io.spring.dependency-management") version "1.1.7"
+
 }
 
 group = "ru.mediatel.icc"
@@ -19,6 +20,10 @@ tasks {
 	}
 }
 
+configurations {
+	create("jooqGenerator")
+}
+
 repositories {
 	mavenCentral()
 }
@@ -29,6 +34,8 @@ dependencies {
 	implementation("org.springframework.boot:spring-boot-starter-web")
 	implementation("org.postgresql:postgresql:42.7.3")
 
+	"jooqGenerator"("org.jooq:jooq-codegen:3.19.24")
+	"jooqGenerator"("org.postgresql:postgresql:42.7.3")
 
 	testImplementation("org.springframework.boot:spring-boot-starter-test")
 	testRuntimeOnly("org.junit.platform:junit-platform-launcher")
@@ -36,4 +43,12 @@ dependencies {
 
 tasks.withType<Test> {
 	useJUnitPlatform()
+}
+
+tasks.register<JavaExec>("generateJooq") {
+	group = "jooq"
+	description = "Generate JOOQ code from XML configuration"
+	mainClass.set("org.jooq.codegen.GenerationTool")
+	classpath = configurations["jooqGenerator"]
+	args = listOf("${projectDir}/src/main/resources/jooq-config.xml")
 }
